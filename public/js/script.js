a3007ec8cad05e333fb2b01b9c523aeacdf785d8
Riwan Carpentier <riwan.carpentier@hotmail.com>
2017-11-30 11:56:07 +0100

    preparing custom markers

--- public/js/script.js
+++ public/js/script.js
@@ -27,8 +27,15 @@ const gInterface = {
                 let data = google.visualization.arrayToDataTable(arrayData);
 
                 let options = {
+                    mapType: 'styledMap',
                     showTooltip: true,
-                    showInfoWindow: true
+                    showInfoWindow: true,
+                    icons: {
+                        default: {
+                            normal: 'https://icons.iconarchive.com/icons/icons-land/vista-map-markers/48/Map-Marker-Ball-Azure-icon.png',
+                            selected: 'https://icons.iconarchive.com/icons/icons-land/vista-map-markers/48/Map-Marker-Ball-Right-Azure-icon.png'
+                        }
+                    }
                 };
 
                 let map = new google.visualization.Map(document.getElementById('map'));
