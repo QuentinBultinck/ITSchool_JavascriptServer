da023b3d4f031da14a129ff7d09ef8ba9f6ba86a
Riwan Carpentier <riwan.carpentier@hotmail.com>
2017-11-30 11:58:40 +0100

    map instead of satellite photo

--- public/js/script.js
+++ public/js/script.js
@@ -35,7 +35,26 @@ const gInterface = {
                             normal: 'https://icons.iconarchive.com/icons/icons-land/vista-map-markers/48/Map-Marker-Ball-Azure-icon.png',
                             selected: 'https://icons.iconarchive.com/icons/icons-land/vista-map-markers/48/Map-Marker-Ball-Right-Azure-icon.png'
                         }
-                    }
+                    },
+                    maps: {
+                        // Your custom mapTypeId holding custom map styles.
+                        styledMap: {
+                            name: 'Styled Map', // This name will be displayed in the map type control.
+                            styles: [
+                                {featureType: 'poi.attraction',
+                                    stylers: [{color: '#fce8b2'}]
+                                },
+                                {featureType: 'road.highway',
+                                    stylers: [{hue: '#0277bd'}, {saturation: -50}]
+                                },
+                                {featureType: 'road.highway',
+                                    elementType: 'labels.icon',
+                                    stylers: [{hue: '#000'}, {saturation: 100}, {lightness: 50}]
+                                },
+                                {featureType: 'landscape',
+                                    stylers: [{hue: '#259b24'}, {saturation: 10}, {lightness: -22}]
+                                }
+                            ]}}
                 };
 
                 let map = new google.visualization.Map(document.getElementById('map'));
